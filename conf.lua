3967da588b9817373ca1033c95f3ffc06ac7d565
kadroff <marsel.kadroff@gmail.com>
2016-08-20 14:42:20 +0500

    alpha 0.01

--- conf.lua
+++ conf.lua
@@ -3,7 +3,7 @@ function love.conf(t)
   t.title = "Pizza Hotline"   -- The title of the window the game is in (string)
   t.version = "0.10.1"        -- The LÖVE version this game was made for (string)
   t.window.width = 480        -- we want our game to be long and thin.
-  t.window.height = 800
+  t.window.height = 600
 
   -- For Windows debugging
   t.console = true
--- main.lua
+++ main.lua
@@ -1,3 +1,17 @@
+debug = true
+
+player = { x = 200, y = 510 }
+
+function love.load()
+  
+end
+
+function love.update(dt)
+  
+end
+
 function love.draw()
-  love.graphics.print("Hello World", 100, 100)
+ -- let's draw our hero
+ love.graphics.setColor(255,255,255,255)
+ love.graphics.rectangle("fill", player.x, player.y, 30, 15)
 end
