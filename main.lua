5b452cf5486c2766557f081fc6be579064dd9ce9
kadroff <marsel.kadroff@gmail.com>
2016-08-20 21:23:45 +0500

    Бета 1.0.0

--- main.lua
+++ main.lua
@@ -1,5 +1,16 @@
 debug = true
 
+-- Collision detection taken function from http://love2d.org/wiki/BoundingBox.lua
+-- Returns true if two boxes overlap, false if they don't
+-- x1,y1 are the left-top coords of the first box, while w1,h1 are its width and height
+-- x2,y2,w2 & h2 are the same, but for the second box
+function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
+  return x1 < x2+w2 and
+         x2 < x1+w1 and
+         y1 < y2+h2 and
+         y2 < y1+h1
+end
+
 player = { 
   x = 200, 
   y = 510, 
@@ -7,6 +18,9 @@ player = {
   width = 40, 
   height = 30 
 }
+
+isAlive = true
+score = 0
 
 -- Таймеры
 -- Определяем переменные здесь,чтобы use их в ф-ях
@@ -16,6 +30,13 @@ canShootTimer = canShootTimerMax
 
 -- Хранилище частиц
 bullets = {} -- массив пуль рисуется и обновляется
+
+ -- Создание врага
+createEnemyTimerMax = 0.4
+createEnemyTimer = createEnemyTimerMax
+  
+-- Хранилище врагов
+enemies = {} -- массив врагов рисуется и обновляется
 
 function love.load()
   
@@ -66,16 +87,88 @@ function love.update(dt)
       table.remove(bullets, i)
     end
   end
+  
+  
+  -- Таймер на создание врагов
+  createEnemyTimer = createEnemyTimer - (1 * dt)
+  if createEnemyTimer < 0 then
+    createEnemyTimer = createEnemyTimerMax
+
+    -- Создание врага
+    randomNumber = math.random(10, love.graphics.getWidth() - 10)
+    newEnemy = { x = randomNumber, y = -10, width = 40, height = 30 }
+    table.insert(enemies, newEnemy)
+  end
+  
+  -- Двигаем врага
+  for i, enemy in ipairs(enemies) do
+    enemy.y = enemy.y + (200 * dt)
+
+    if enemy.y > 850 then -- удаляем когда он зашел за экран
+      table.remove(enemies, i)
+    end
+  end
+  
+  -- run our collision detection
+  -- Since there will be fewer enemies on screen than bullets we'll loop them first
+  -- Also, we need to see if the enemies hit our player
+  for i, enemy in ipairs(enemies) do
+    for j, bullet in ipairs(bullets) do
+      if CheckCollision(enemy.x, enemy.y, enemy.width, enemy.height, bullet.x, bullet.y, bullet.width, bullet.height) then
+        table.remove(bullets, j)
+        table.remove(enemies, i)
+        score = score + 1
+      end
+    end
+
+    if CheckCollision(enemy.x, enemy.y, enemy.width, enemy.height, player.x, player.y, player.width, player.height) 
+    and isAlive then
+      table.remove(enemies, i)
+      isAlive = false
+    end
+  end
+  
+  if not isAlive and love.keyboard.isDown('r') then
+    -- remove all our bullets and enemies from screen
+    bullets = {}
+    enemies = {}
+
+    -- reset timers
+    canShootTimer = canShootTimerMax
+    createEnemyTimer = createEnemyTimerMax
+
+    -- move player back to default position
+    player.x = 50
+    player.y = 510
+
+    -- reset our game state
+    score = 0
+    isAlive = true
+  end
 end
 
 function love.draw()
-  -- рисуем нашего персонажа
-  love.graphics.setColor(255,255,255,255)
-  love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
+  if isAlive then
+    -- рисуем нашего персонажа
+    love.graphics.setColor(255,255,255,255)
+    love.graphics.rectangle("fill", player.x, player.y, player.width, player.height)
+  else
+    love.graphics.print("Нажмите 'R' начать заново", love.graphics:getWidth()/2-50, love.graphics:getHeight()/2-10)
+  end
+  
  
   -- рисуем пули
   for i, bullet in ipairs(bullets) do
     love.graphics.setColor(255,255,255,255)
     love.graphics.rectangle("fill", bullet.x, bullet.y, bullet.width, bullet.height)
   end
+  
+  -- Рисуем врагов
+  for i, enemy in ipairs(enemies) do
+    love.graphics.setColor(255,255,0,255)
+    love.graphics.rectangle("fill", enemy.x, enemy.y, enemy.width, enemy.height)
+  end
+  
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.print("SCORE: " .. tostring(score), 400, 10)
 end
